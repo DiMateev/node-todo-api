268bb83c15fe10cd53c27d0bd17f864a2178fa29
Dimitar Mateev <mateevpro@gmail.com>
2017-06-30 12:41:15 +0300

    Refractorin API

---
+++ server/models/todo.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var TodoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+var Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports = {Todo};
